8f8e3bd7c6ab7203a0866a05373d1098a1e40a8e
Alexey Abdusamatov <xyzuser@yandex.ru>
2019-07-07 16:51:57 +0300

    HW#6 part3 completed

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,11 +12,10 @@ provider "google" {
 }
 
 resource "google_compute_project_metadata" "default" {
-	metadata = {
-   		ssh-keys = "appuser1:${file(var.public_key_path)}\nappuser2:${file(var.public_key_path)}"
- 	}
+  metadata = {
+    ssh-keys = "appuser1:${file(var.public_key_path)}\nappuser2:${file(var.public_key_path)}"
+  }
 }
-
 
 resource "google_compute_instance" "app" {
   name         = "reddit-app"
@@ -43,9 +42,53 @@ resource "google_compute_instance" "app" {
   metadata {
     #путь до публичного ключа
     ssh-keys = "appuser:${file(var.public_key_path)}"
-
   }
 
+  connection {
+    type  = "ssh"
+    user  = "appuser"
+    agent = false
+
+    # путь до приватного ключа
+    private_key = "${file(var.private_key)}"
+  }
+
+  provisioner "file" {
+    source      = "files/puma.service"
+    destination = "/tmp/puma.service"
+  }
+
+  provisioner "remote-exec" {
+    script = "files/deploy.sh"
+  }
+}
+
+resource "google_compute_instance" "app2" {
+  name         = "reddit-app2"
+  machine_type = "g1-small"
+  zone         = "europe-west1-b"
+  tags         = ["reddit-app"]
+
+  # определение загрузочного диска
+  boot_disk {
+    initialize_params {
+      image = "${var.disk_image}"
+    }
+  }
+
+  # определение сетевого интерфейса
+  network_interface {
+    # сеть, к которой присоединить данный интерфейс
+    network = "default"
+
+    # использовать ephemeral IP для доступа из Интернет
+    access_config {}
+  }
+
+  metadata {
+    #путь до публичного ключа
+    ssh-keys = "appuser:${file(var.public_key_path)}"
+  }
 
   connection {
     type  = "ssh"
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,4 +1,11 @@
-output"app_external_ip"{
- value="${google_compute_instance.app.network_interface.0.access_config.0.nat_ip}"
+output "app_external_ip" {
+  value = "${google_compute_instance.app.network_interface.0.access_config.0.nat_ip}"
 }
 
+output "app_external_ip2" {
+  value = "${google_compute_instance.app2.network_interface.0.access_config.0.nat_ip}"
+}
+
+output "lb_ip" {
+  value = "${google_compute_global_forwarding_rule.reddit_fwd_rule.ip_address}"
+}
